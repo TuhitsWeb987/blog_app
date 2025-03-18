09401077dc0c7df3314c53df9c97963d3a0bb349
TuhitsWeb987 <tuhiti.falchetto.web@gmail.com>
2025-03-17 21:43:14 -1000

    third commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -8,10 +9,25 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', [
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('post.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+    Route::post('/post/{post}/like', [PostController::class, 'like'])->name('post.like');
+})
+
+Route::get('/dashboard', function(){
+    return Inertia::render('Dashboard', [
         'usersPost' => Auth::user()->posts()->with('author')->latest()->get()
     ]);
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
