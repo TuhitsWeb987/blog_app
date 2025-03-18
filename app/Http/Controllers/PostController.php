d8b554ec6b8a46952a992a721589b3da1304da1b
TuhitsWeb987 <tuhiti.falchetto.web@gmail.com>
2025-03-17 21:32:47 -1000

    seconde commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Response;
 use Inertia\Inertia;
 
@@ -51,8 +52,55 @@ class PostController extends Controller
 
     public function edit(Post $post): Response {
         return Inertia::render('Posts/Edit', [
-            'post' => $post->load('author')
+            'post' => $post
         ]);
+    }
+
+    public function update(Request $request, $post) {
+
+        $validated = $request->validate([
+            'title' => 'required|string|string:255',
+            'description' => 'required|string',
+            'image' => 'nullable|image|mimes:png,jpg,jpeg,svg,gif|max:2048',
+        ]);
+
+        $post->title = $validated['title'];
+        $post->description = $validated['description'];
+
+        if($request->hasFile('image')) {
+            if($post->image) {
+                Storage::disk('public')->delete($post->image);
+            }
+            $path = $request->file('image')->store('posts', 'public');
+            $post->image = $path;
+        }
+
+        $post->save();
+
+        return redirect()->route('dashboard')->with('sucess', 'Post modifié avec succès');
+    }
+
+    public function destroy(Post $post) {
+        if($post->image) {
+            Storage::disk('public')->delete($post->image);
+        }
+        
+        $post->delete();
+
+        return redirect()->back()->with('success', 'Post supprimé avec succès');
+    }
+
+    public function like(Post $post) {
+        $user = Auth::user();
+        if($post->likedBy()->where('user_id', $user->id)->exists()) {
+            $post->likedBy()->detach($user->id);
+            $message = 'Post retiré';
+        } else {
+            $post->likedBy()->attach($user->id);
+            $message = 'Post liké';
+        }
+
+        return redirect()->back()->with('success', $message);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -8,9 +9,9 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', [
+        'usersPost' => Auth::user()->posts()->with('author')->latest()->get()
+    ]);
 });
 
 require __DIR__.'/settings.php';
